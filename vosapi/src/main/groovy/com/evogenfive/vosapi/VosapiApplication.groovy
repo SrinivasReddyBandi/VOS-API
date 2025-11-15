14c369dd918c099a0e26fd22dc53864b47c2d210
unknown <ishaik@evoketechnologies.com>
2025-11-15 07:15:03 +0000

    Initial commit

---
+++ vosapi/src/main/groovy/com/evogenfive/vosapi/VosapiApplication.groovy
@@ -0,0 +1,13 @@
+package com.evogenfive.vosapi
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class VosapiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(VosapiApplication, args)
+	}
+
+}
