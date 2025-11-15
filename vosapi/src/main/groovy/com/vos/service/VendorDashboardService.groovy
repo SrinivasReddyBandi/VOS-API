e9ae36051a5da9278112a2509ec7ce8c4fa7eae6
ishaik-evoke <evoke@evoketechnologies.com>
2025-11-15 11:59:55 +0000

    code changes

--- vosapi/src/main/groovy/com/vos/service/VendorDashboardService.groovy
+++ vosapi/src/main/groovy/com/vos/service/VendorDashboardService.groovy
@@ -2,6 +2,7 @@ package com.vos.service
 
 import com.vos.enums.VendorStatus
 import com.vos.model.dto.*
+import org.springframework.core.io.Resource
 
 interface VendorDashboardService {
     PageResponse<VendorResponseDto> getVendors(
@@ -16,5 +17,11 @@ interface VendorDashboardService {
     VendorDetailDto getVendorDetail(Long vendorId)
     List<ActivityLogDto> getVendorActivityLog(Long vendorId)
     VendorMetricsDto getMetrics()
-}
 
+    List<VendorFileDto> getVendorFiles(Long vendorId)
+    VendorFileDto getVendorFile(Long vendorId, Long fileId)
+    // returns a Spring Resource for download
+    Resource downloadFile(Long vendorId, Long fileId)
+    // re-run validation on an existing file
+    VendorFileDto revalidateFile(Long vendorId, Long fileId)
+}
